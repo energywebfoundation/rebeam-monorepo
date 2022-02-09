9c037b12f21feda2ede3911712af4f0276eb3126
whitneypurdum <whitney.purdum@energyweb.org>
2022-02-09 17:51:19 -0500

    fix controller unit test - create instance of cache

--- packages/emsp-backend/src/presentation/presentation.controller.spec.ts
+++ packages/emsp-backend/src/presentation/presentation.controller.spec.ts
@@ -1,10 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PresentationController } from './presentation.controller';
 import { CacheModule, Logger } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/common';
+
 import { LoggerService } from '../logger/logger.service';
 import { PresentationService } from './presentation.service';
 describe('PresentationController', () => {
   let presController: PresentationController;
+  let cache: Cache;
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [PresentationController],
@@ -17,9 +21,11 @@ describe('PresentationController', () => {
     }).compile();
 
     presController = app.get<PresentationController>(PresentationController);
+    cache = app.get<Cache>(CACHE_MANAGER)
   });
 
   describe('presentation', () => {
+    
     const presentationInfo = {
       presentationLink: {
         type: 'string',
@@ -30,12 +36,22 @@ describe('PresentationController', () => {
     };
     it('should return the presentation information that is cached', async () => {
       const result = await presController.cachePresentation(presentationInfo);
-      expect(result).toBe(presentationInfo);
+      expect(result).toEqual(presentationInfo);
     });
     it('should fetch the cache given a uuid', async () => {
-      const id = presentationInfo.ocpiTokenUID;
-      const result = await presController.fetchPresentationData(id);
-      expect(result).toBe(presentationInfo);
+      const data = {
+        presentationLink: {
+          type: 'string',
+          url: 'string',
+          ssiSession: 'string',
+        },
+        ocpiTokenUID: 'test6',
+      }
+      
+      const {ocpiTokenUID} = data;
+      await cache.set(ocpiTokenUID, data)
+      const result = await presController.fetchPresentationData(ocpiTokenUID);
+      expect(result).toEqual(data);
     });
   });
 });
