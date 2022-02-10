b5f2b03c04ffbeb0e1231d8ef1f5632bb7e59303
whitneypurdum <whitney.purdum@energyweb.org>
2022-02-10 11:21:09 -0500

    remove uuid package

--- packages/emsp-backend/src/charge/charge.service.ts
+++ packages/emsp-backend/src/charge/charge.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { v4 as uuidv4 } from 'uuid';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class ChargeService {
   async initiate(): Promise<string> {
     //Placeholder for call to Charge Operator to get a start session token:
-    const mockOcpiToken = uuidv4();
+    const mockOcpiToken = randomUUID();
     console.log(mockOcpiToken);
     return mockOcpiToken;
   }
