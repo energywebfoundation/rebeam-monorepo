11956be3a271c6e1c7e66fa01e4cb619bd2ff3e9
whitneypurdum <whitney.purdum@energyweb.org>
2022-03-23 11:32:35 -0400

    add error message for no locations found

--- packages/emsp-backend/src/location/location.service.ts
+++ packages/emsp-backend/src/location/location.service.ts
@@ -20,14 +20,17 @@ export class LocationService {
     };
     const locations = await this.bridge.requests.getLocations(recipient);
     const { data } = locations;
-    const locationsFormatted = data?.map((data) => {
-      const evseStringified = JSON.stringify(data.evses);
-      return { ...data, evses: evseStringified };
-    });
-    const insertResult = await this.locationDbService.insertLocations(
-      locationsFormatted
-    );
-    return insertResult;
+    if (data) {
+        const locationsFormatted = data?.map((data) => {
+            const evseStringified = JSON.stringify(data.evses);
+            return { ...data, evses: evseStringified };
+          });
+          const insertResult = await this.locationDbService.insertLocations(
+            locationsFormatted
+          );
+          return insertResult;
+    } else throw new Error("No locations found for given country code and party id")
+ 
   }
 
   async fetchLocationsForClient(): Promise<ClientLocationsDTO> {
