ba690d0dd66e8c906b047797c150e0aa6a1c3c5d
whitneypurdum <whitney.purdum@energyweb.org>
2022-03-25 10:54:04 -0400

    center popover content for supplier wallet

--- packages/ev-charging-app/src/components/WalletPopover.tsx
+++ packages/ev-charging-app/src/components/WalletPopover.tsx
@@ -73,13 +73,9 @@ const WalletPopover = (props: IWalletPopoverProps) => {
   return (
     <>
       <IonPopover
-        alignment="end"
-        side="end"
         isOpen={isOpen}
-        size={'cover'}
-        trigger="start-charging"
         showBackdrop={true}
-        reference="trigger"
+        onDidDismiss={() => setSupplierModal(false)}
       >
         {/* <div> */}
         <IonContent>
