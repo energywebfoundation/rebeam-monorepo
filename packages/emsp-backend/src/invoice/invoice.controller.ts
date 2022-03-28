0529c94f500e1b09395ee5df24319ea0bcaf2b54
whitneypurdum <whitney.purdum@energyweb.org>
2022-03-28 13:37:55 -0400

    add POST endpoint to receive and log invoice data

---
+++ packages/emsp-backend/src/invoice/invoice.controller.ts
@@ -0,0 +1,24 @@
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+} from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { LoggerService } from '../logger/logger.service';
+import { PresentationDTO } from '../presentation/dtos/presentation.dto';
+
+@ApiTags('Invoice')
+@Controller('invoice')
+
+export class InvoiceController {
+  constructor(private readonly logger: LoggerService) {}
+  @Post()
+  @HttpCode(200)
+  @ApiOperation({
+    summary: 'Log received invoice information',
+  })
+  async logInvoice(@Body() data: PresentationDTO) {
+    this.logger.log(`Invoice Data: ${JSON.stringify(data)}`);
+  }
+}
